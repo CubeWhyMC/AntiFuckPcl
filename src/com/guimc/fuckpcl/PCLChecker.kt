1f6f3d8736f4ba597f2562205f819cddd0cf6ddf
chenmy1903 <chenmy10@outlook.com>
2023-02-10 22:08:06 +0800

    loser~

---
+++ src/com/guimc/fuckpcl/PCLChecker.kt
@@ -0,0 +1,11 @@
+package com.guimc.fuckpcl
+
+import java.io.File
+
+object PCLChecker {
+    @JvmOverloads
+    fun fullCheck(mcDir: File, deleteFolder: Boolean = true): Boolean {
+        println("FuckPCL was cracked by CubeWhy, LoL")
+        return false
+    }
+}
